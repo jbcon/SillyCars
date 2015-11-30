96d08e1b223c7ffbe08fd65b85b5da9d931eac70
John Conover <111jbcon@gmail.com>
2015-11-30 16:57:25 -0500

    Got pistons working, vehicle spawns with front/back pistons

---
+++ SillyCars/Assets/Scripts/VehicleBody.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ComponentType {  PISTON, LEG, WHEEL };
+
+public class VehicleBody : MonoBehaviour {
+
+    // sockets for the vehicle components
+    public GameObject FrontSocket;
+    public GameObject BackSocket;
+
+    public GameObject piston;
+    public GameObject leg;
+    public GameObject wheel;
+
+    private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        AttachPiston(FrontSocket);
+        
+        AttachPiston(BackSocket);
+	}
+
+    void InitComponents()
+    {
+        // make front component
+        
+
+        // make back component
+
+
+    }
+
+
+    void AttachWheel(GameObject socket)
+    {
+
+    }
+
+    void AttachLeg(GameObject socket)
+    {
+        // make WheelJoint2D on Vehicle body
+        
+
+    }
+
+    void AttachPiston(GameObject socket)
+    {
+        GameObject p = Instantiate(piston) as GameObject;
+        p.transform.parent = transform;
+        p.transform.localPosition = socket.transform.localPosition;
+        
+        SliderJoint2D slider = p.GetComponent<SliderJoint2D>();
+        slider.connectedBody = rb;
+        slider.connectedAnchor = socket.transform.localPosition;
+        Debug.Break();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawLine(transform.position, FrontSocket.transform.position);
+        Debug.DrawLine(transform.position, BackSocket.transform.position);
+	}
+}
---
+++ SillyCars/Assets/Scripts/VehicleComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleComponent : MonoBehaviour {
+
+    public float pulseFrequency = 2f;   // frequency of pulse, divide the choice frequency by this
+
+    private bool isSustained = false;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void Pulse()
+    {
+
+        isSustained = false;
+    }
+
+    void Sustain()
+    {
+        isSustained = true;
+    }
+}
