454a09a9e02c37ecaf269cd90fd12823222acf22
Robert Rouhani <robert.rouhani@gmail.com>
2015-12-02 20:55:30 -0500

    Fixed minor bugs

--- SillyCars/Assets/Scripts/VehicleBody.cs
+++ SillyCars/Assets/Scripts/VehicleBody.cs
@@ -5,33 +5,33 @@ public enum ComponentType {  PISTON, LEG, WHEEL };
 
 public class VehicleBody : MonoBehaviour {
 
-    // sockets for the vehicle components
-    public GameObject FrontSocket;
-    public GameObject BackSocket;
+	// sockets for the vehicle components
+	public GameObject FrontSocket;
+	public GameObject BackSocket;
 
-    public LocomotionComponent frontControl;
-    public LocomotionComponent backControl;
+	public LocomotionComponent frontControl;
+	public LocomotionComponent backControl;
 
-    public GameObject piston;
-    public GameObject leg;
-    public GameObject wheel;
+	public GameObject piston;
+	public GameObject leg;
+	public GameObject wheel;
 
-    public bool debugInputEnabled = true;
+	public bool debugInputEnabled = true;
 
-    private Rigidbody2D rb;
+	private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-        rb = GetComponent<Rigidbody2D>();
-        InitComponents();
+		rb = GetComponent<Rigidbody2D>();
+		InitComponents();
 	}
 
-    void InitComponents()
-    {
-        // make front component
-        frontControl = AttachLeg(FrontSocket);
-        // make back component
-        backControl = AttachPiston(BackSocket);
+	void InitComponents()
+	{
+		// make front component
+		frontControl = AttachLeg(FrontSocket);
+		// make back component
+		backControl = AttachPiston(BackSocket);
 
 		Driver driverComp = GetComponent<Driver>();
 
@@ -40,70 +40,73 @@ public class VehicleBody : MonoBehaviour {
 			driverComp.frontSwitch = frontControl;
 			driverComp.backSwitch = backControl;
 		}
-    }
+	}
 
 
-    void AttachWheel(GameObject socket)
-    {
+	void AttachWheel(GameObject socket)
+	{
 
-    }
+	}
 
-    LocomotionComponent AttachLeg(GameObject socket)
-    {
-        // make WheelJoint2D on Vehicle body
-        WheelJoint2D legJoint = gameObject.AddComponent<WheelJoint2D>();
-        GameObject l = Instantiate(leg) as GameObject;
+	LocomotionComponent AttachLeg(GameObject socket)
+	{
+		// make WheelJoint2D on Vehicle body
+		WheelJoint2D legJoint = gameObject.AddComponent<WheelJoint2D>();
+		GameObject l = Instantiate(leg) as GameObject;
 
-        l.transform.parent = transform;
-        l.transform.localPosition = socket.transform.localPosition;
-        legJoint.connectedBody = l.GetComponent<Rigidbody2D>();
-        legJoint.connectedAnchor = l.transform.localPosition;
-        legJoint.anchor = socket.transform.localPosition;
-        legJoint.useMotor = true;
-        JointSuspension2D sus = new JointSuspension2D();
-        sus.frequency = 5;
-        sus.dampingRatio = 1;
-        legJoint.suspension = sus;
-        JointMotor2D newMotor = new JointMotor2D();
-        newMotor.motorSpeed = -300;
-        newMotor.maxMotorTorque = 100;
-        legJoint.motor = newMotor;
+		l.transform.parent = transform;
+		l.transform.localPosition = socket.transform.localPosition;
+		legJoint.connectedBody = l.GetComponent<Rigidbody2D>();
+		legJoint.connectedAnchor = l.transform.localPosition;
+		legJoint.anchor = socket.transform.localPosition;
+		legJoint.useMotor = true;
+		JointSuspension2D sus = new JointSuspension2D();
+		sus.frequency = 5;
+		sus.dampingRatio = 1;
+		legJoint.suspension = sus;
+		JointMotor2D newMotor = new JointMotor2D();
+		newMotor.motorSpeed = -300;
+		newMotor.maxMotorTorque = 100;
+		legJoint.motor = newMotor;
 
-        SpinningLeg spinleg = l.GetComponent<SpinningLeg>();
-        spinleg.SetRefToJoint(legJoint);
-        return spinleg;
-    }
+		SpinningLeg spinleg = l.GetComponent<SpinningLeg>();
+		spinleg.SetRefToJoint(legJoint);
+		return spinleg;
+	}
 
-    LocomotionComponent AttachPiston(GameObject socket)
-    {
-        GameObject p = Instantiate(piston) as GameObject;
-        p.transform.parent = transform;
-        p.transform.localPosition = socket.transform.localPosition;
-        
-        SliderJoint2D slider = p.GetComponent<SliderJoint2D>();
-        slider.connectedBody = rb;
-        slider.connectedAnchor = socket.transform.localPosition;
+	LocomotionComponent AttachPiston(GameObject socket)
+	{
+		GameObject p = Instantiate(piston) as GameObject;
+		p.transform.parent = transform;
+		p.transform.localPosition = socket.transform.localPosition;
+		
+		SliderJoint2D slider = p.GetComponent<SliderJoint2D>();
+		slider.connectedBody = rb;
+		slider.connectedAnchor = socket.transform.localPosition;
 
-        return p.GetComponent<Piston>();
-    }
+		return p.GetComponent<Piston>();
+	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Fire1"))
-        {
-            frontControl.On();
-        }
-        else
-        {
-            frontControl.Off();
-        }
-        if (Input.GetButton("Fire2"))
-        {
-            backControl.On();
-        }
-        else
-        {
-            backControl.Off();
-        }
+		if (debugInputEnabled)
+		{
+			if (Input.GetButton("Fire1"))
+			{
+				frontControl.On();
+			}
+			else
+			{
+				//frontControl.Off();
+			}
+			if (Input.GetButton("Fire2"))
+			{
+				backControl.On();
+			}
+			else
+			{
+				//backControl.Off();
+			}
+		}
 	}
 }
